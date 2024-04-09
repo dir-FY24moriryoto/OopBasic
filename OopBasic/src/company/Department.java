70ef6abd6decb16621b3ad2c9e5b07c1fd077a94
dir-FY24moriryoto <ryoto.mori.p942@dirbato.co.jp>
2024-04-09 13:01:21 +0900

    chpter5まで

--- OopBasic/src/company/Department.java
+++ OopBasic/src/company/Department.java
@@ -1,5 +1,42 @@
 package company;
 
-public class Department {
+import java.lang.invoke.StringConcatFactory;
 
+public class Department {
+    
+    //フィールド
+    private final String name;
+    private final String departmentId;
+    private final int budget;
+    
+    //コンストラクター
+    public Department(String name,String departmentId,int budget) {
+        this.name = name;
+        this.departmentId = departmentId;
+        this.budget = budget;
+    }
+    
+    //操作の定義（やってもらいたいこと）、メソッド＝会議の主催
+    public void meeting() {
+        System.out.println("部内会議を開催します。部署:" + name + "、予算：" + budget);
+    }
+    
+    //nameのgetter
+    public String getName() {
+        return name;
+    }
+    
+    //budgetのgetter
+    public double getBudget() {
+        return budget;
+    }
+    
+    
+    //budgetのsetter
+    public void setBudget(int budget) {
+        if(budget < 0) {
+            throw new IllegalAccessException("予算はマイナスにできません。");
+        }
+        this.budget = budget;
+    }
 }
--- OopBasic/src/company/ObjectSample.java
+++ OopBasic/src/company/ObjectSample.java
@@ -4,17 +4,18 @@ public class ObjectSample {
 
     public static void main(String[] args) {
         //インスタンスの作成
-        var employee = new Employee("鈴木","営業部","課長",100);
+        var department = new Department("営業部", "xx", 1000000);
+        var employee = new Employee("鈴木",department,"課長",100);
         
         //インスタンスメソッドの呼び出し
         employee.report();
         employee.report(2);
         
         System.out.println("");
-        var engineer = new Employee("田中", "開発部", "一般社員", 88);
+        var engineer = new Employee("田中", department, "一般社員", 88);
         
         engineer.report();
-
+        engineer.joinMeeting();
     }
 
 }
